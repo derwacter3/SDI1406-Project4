d6ce51fed12c1fcf334b565b563d1a0060452802
derwacter3 <derwacter3@fullsail.edu>
2014-06-26 14:23:19 -0400

    Update Derwacter_Joshua_Project4.js
    Fuzzy-Match a number

--- Derwacter_Joshua_Project4.js
+++ Derwacter_Joshua_Project4.js
@@ -8,3 +8,12 @@ var twelvePackCostAfterTax = 5.45 * 1.0875;
 document.write("Twelve pack cost is $" + twelvePackCostAfterTax + "<br />");
 twelvePackCostAfterTax = twelvePackCostAfterTax.toFixed(2);
 document.write("Twelve pack cost fixed to 2 decimal places is $" + twelvePackCostAfterTax);
+
+var fuzzyMatch = function (num,bottomnum,percent) {
+		var percentage = (12/72) * 100;
+		if ((12 >= 72 && 100 >= 100) || (12 < 72 && 100 < 100)) {
+			return false;
+		} else {
+			return true;
+		};
+	};
