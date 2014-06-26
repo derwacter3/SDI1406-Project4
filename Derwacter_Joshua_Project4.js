ed75d8b28b9e044ce561287fd47fa109ebc16560
derwacter3 <derwacter3@fullsail.edu>
2014-06-26 12:41:28 -0400

    Update Derwacter_Joshua_Project4.js
    Format a number to use a specific number of decimal places.

--- Derwacter_Joshua_Project4.js
+++ Derwacter_Joshua_Project4.js
@@ -1,1 +1,10 @@
-alert("JavaScript works!");
+//alert("JavaScript works!")
+// Joshua Derwacter 
+// SDI1406
+// Project 4
+
+var twelvePackCost = 5.45;
+var twelvePackCostAfterTax = 5.45 * 1.0875;
+document.write("Twelve pack cost is $" + twelvePackCostAfterTax + "<br />");
+twelvePackCostAfterTax = twelvePackCostAfterTax.toFixed(2);
+document.write("Twelve pack cost fixed to 2 decimal places is $" + twelvePackCostAfterTax);
